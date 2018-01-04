9258100541ce4d3c85259bf1e2c7625a6016ca05
SandeepKumarPN <sandeepkumarpn1234@gmail.com>
2018-01-04 17:07:13 +0530

    initial commits

---
+++ PrivatePod/MyCalc.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalc.swift
+//  PrivatePod
+//
+//  Created by Naresh on 04/01/18.
+//  Copyright © 2018 Naresh. All rights reserved.
+//
+
+import UIKit
+
+public class MyCalc: NSObject {
+
+    public func add(a:Int,b:Int) -> Int {
+        return a + b
+        ////
+    }
+}
---
+++ PrivatePod/PrivatePod.h
@@ -0,0 +1,19 @@
+//
+//  PrivatePod.h
+//  PrivatePod
+//
+//  Created by Naresh on 04/01/18.
+//  Copyright © 2018 Naresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrivatePod.
+FOUNDATION_EXPORT double PrivatePodVersionNumber;
+
+//! Project version string for PrivatePod.
+FOUNDATION_EXPORT const unsigned char PrivatePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivatePod/PublicHeader.h>
+
+
